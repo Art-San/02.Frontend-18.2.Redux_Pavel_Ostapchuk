df74f7dc1c608326e888ac354ef0901c9e9ac6b7
Aleksander <asava@sibmail.com>
2023-05-31 17:34:14 +0700

    без редакс

--- src/entities/UsersPage/UsersPage.jsx
+++ src/entities/UsersPage/UsersPage.jsx
@@ -1,39 +1,39 @@
-// import { useState } from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-// import { $api } from '../../api/axios-instance'
-import { featchUsersAsync } from '../../store/UsersPage/actions'
-import { getUsersStateSelector } from '../../store/UsersPage/selectors'
+import { useState, useEffect } from 'react'
+// import { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+import { $api } from '../../api/axios-instance'
+// import { featchUsersAsync } from '../../store/UsersPage/actions'
+// import { getUsersStateSelector } from '../../store/UsersPage/selectors'
 
 const UsersPage = () => {
   // withour redux
-  // const [usersList, setUsersList] = useState([])
-  // const [isLoading, setIsLoading] = useState(false)
-  // console.log(usersList)
-  // const getUsers = async () => {
-  //   try {
-  //     setIsLoading(true)
-  //     const { data } = await $api.get('/users')
-  //     setUsersList(data)
-  //     setIsLoading(false)
-  //   } catch (error) {
-  //     setIsLoading(false)
-  //     console.error('Ошибка:', error.message)
-  //   }
-  // }
-  // useEffect(() => {
-  //   getUsers()
-  // }, [])
+  const [usersList, setUsersList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  console.log(usersList)
+  const getUsers = async () => {
+    try {
+      setIsLoading(true)
+      const { data } = await $api.get('/users')
+      setUsersList(data)
+      setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+      console.error('Ошибка:', error.message)
+    }
+  }
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   // Vanilla Redux
-  const dispatch = useDispatch()
-  const { userData: usersList, loading: isLoading } = useSelector(
-    getUsersStateSelector
-  )
+  // const dispatch = useDispatch()
+  // const { userData: usersList, loading: isLoading } = useSelector(
+  //   getUsersStateSelector
+  // )
 
-  useEffect(() => {
-    dispatch(featchUsersAsync())
-  }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(featchUsersAsync())
+  // }, [dispatch])
 
   return isLoading ? (
     <div>Загрузка...</div>
